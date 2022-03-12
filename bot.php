da1630c37815457da8305ceaad81ef8412b6bafd
Enmanuel <65689762+ysEnmanuel@users.noreply.github.com>
2022-03-12 01:33:25 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,8 +7,6 @@
     $chat_id = $update["message"]["chat"]["id"];
     $username = $update["message"]["chat"]["username"];
     $message = $update["message"]["text"];
-    $from_chat_id = $update["message"]["chat"]["id"]
-    $message_id = $update["message"]["reply_to_message"]["id"]
     
     if($message == "/reenviar"){
         send_message($chat_id, $from_chat_id; $message_id);
@@ -18,7 +16,7 @@
         $number = rand(666,999);
         send_message($chat_id, $number);
     }
-    
+
     if($message == "/time"){
         $masa = date("d/m/y h:i a", time());
         send_message($chat_id, $masa);
