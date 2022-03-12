5e0aaf8ad479f7c066f6ee1807a614aff681240a
Enmanuel <65689762+ysEnmanuel@users.noreply.github.com>
2022-03-12 01:29:11 -0500

    Update bot.php

--- bot.php
+++ bot.php
@@ -7,10 +7,17 @@
     $chat_id = $update["message"]["chat"]["id"];
     $username = $update["message"]["chat"]["username"];
     $message = $update["message"]["text"];
+    $from_chat_id = $update["message"]["chat"]["id"]
+    $message_id = $update["message"]["reply_to_message"]["id"]
     
+    if($message == "/reenviar"){
+        send_message($chat_id, $from_chat_id; $message_id);
+    }
+
     if($message == "/s"){
         send_message(.$message == "");
     }
+
     if($message == "/time"){
         $masa = date("d/m/y h:i a", time());
         send_message($chat_id, $masa);
@@ -20,15 +27,5 @@
         $text = urlencode($msg);
         file_get_contents("https://api.telegram.org/bot5243109805:AAESxkazRdWTgCAiYY0A_9npQScCbYYUapY/sendMessage?chat_id=1054154613&text=$text");
     }
-
-    bot.onText(/^\!reenviar/, (msg) => {
-var chat_id = msg.chat.id;
-var from_chat_id = msg.chat.id;
-var message_id = msg.reply_to_message.message_id
-
- // Método para reenviar el mensaje
- bot.forwardMessage(chat_id, from_chat_id, message_id);
-
-});
     
 ?>
